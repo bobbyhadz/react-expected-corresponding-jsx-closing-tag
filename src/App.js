42bdb57af4408b85418810aa57061464049e98fa
bobbyhadz <opmftw@gmail.com>
2024-04-06 11:30:29 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <input id="icon_prefix" type="text" className="validate" />
+    </div>
+  );
+}
